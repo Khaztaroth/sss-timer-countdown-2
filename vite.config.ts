3e4024a1c4740fc8fc87ee8527a0d54aa1951135
khaztaroth <astaroth503@gmail.com>
2024-06-29 18:29:16 -0400

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import presetAtributify from '@unocss/preset-attributify'
 import presetWind from '@unocss/preset-wind'
 
 export default defineConfig({
+  preview: {
+    proxy: {
+      '/api': {
+        target: 'https://sss-timer-dashboard.khaz.workers.dev/dash',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+  },
   server: {
     proxy: {
       '/api': {
@@ -13,7 +22,7 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
-    }
+    },
   },
   plugins: [
     UnoCSS({
