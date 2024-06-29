fdfd537d8e1e3434704e8924d66fe3a37e781072
khaztaroth <astaroth503@gmail.com>
2024-06-29 18:33:50 -0400

    changed api route

--- src/external/streamInfo.ts
+++ src/external/streamInfo.ts
@@ -1,7 +1,7 @@
 const channelName = 'secretsleepoversociety';
 
 export async function useActiveDays(): Promise<string> {
-    const activeDays = async () => await fetch(`/api`)
+    const activeDays = async () => await fetch(`https://sss-timer-dashboard.khaz.workers.dev/dash`)
     .then((res) => res.text())
 
     return await activeDays()
