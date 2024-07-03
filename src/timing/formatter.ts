3e9ee2731d192b92f729d88f3108752ed1ff28e2
khaztaroth <astaroth503@gmail.com>
2024-07-03 19:06:47 -0400

    unfucked the timer format

--- src/timing/formatter.ts
+++ src/timing/formatter.ts
@@ -4,22 +4,22 @@ export function useFormatter(timeDiff: Duration) {
     if (timeDiff.days > 0) {
         return (
             timeDiff.toFormat(
-                `d '${timeDiff.days === 1 ? 'day' : 'days'}' h '${timeDiff.hours === 1 ? 'hour' : 'hours'}' m '${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
+                `d '${timeDiff.days === 1 ? 'day' : 'days'}' h '${timeDiff.hours === 1 ? 'hour' : 'hours'}' m '${timeDiff.minutes === 0 ? "" : timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
             )
         )
-    } else if (timeDiff.hours > 1) {
+    } else if (timeDiff.hours >= 0) {
         return (
             timeDiff.toFormat(
-                `h '${timeDiff.hours === 1 ? 'hour' : 'hours'}' m '${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
+                `h '${timeDiff.hours === 1 ? 'hour' : 'hours'}' m '${timeDiff.minutes === 0 ? "" : timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
             )
         )
-    } else if (timeDiff.minutes > 1) {
+    } else if (timeDiff.minutes >= 1) {
         return (
             timeDiff.toFormat(
                 `m '${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
             )
         )
-    } else if (timeDiff.seconds > 1) {
+    } else if (timeDiff.seconds >= 0) {
         return (
            timeDiff.toFormat(
             `s ${timeDiff.seconds < 2 ? "'s'econ'd'" : "'s'econ'ds'"}`
