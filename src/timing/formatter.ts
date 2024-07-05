69e1ea5d23dbd6400e95e8d18b47f1ab1461d271
khaztaroth <astaroth503@gmail.com>
2024-07-04 21:04:31 -0400

    removed redundant "0 time" labels

--- src/timing/formatter.ts
+++ src/timing/formatter.ts
@@ -1,28 +1,28 @@
 import { Duration } from "luxon";
 
 export function useFormatter(timeDiff: Duration) {
-    if (timeDiff.days > 0) {
+    if (timeDiff.days >= 1) {
         return (
             timeDiff.toFormat(
-                `d '${timeDiff.days === 1 ? 'day' : 'days'}' h '${timeDiff.hours === 1 ? 'hour' : 'hours'}' m '${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
+                `d '${timeDiff.days < 2 ? 'day' : 'days'}' ${timeDiff.hours >= 1 ? `h '${timeDiff.hours < 2 ? 'hour' : 'hours'}'` : ''} ${timeDiff.minutes >= 1 ? `m '${timeDiff.minutes < 2 ? 'minute' : 'minutes'}'` : ''}`
             )
         )
     } else if (timeDiff.hours > 0) {
         return (
             timeDiff.toFormat(
-                `h '${timeDiff.hours === 1 ? 'hour' : 'hours'}' m '${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
+                `h '${timeDiff.hours < 2 ? 'hour' : 'hours'}' ${timeDiff.minutes >= 1 ? `m '${timeDiff.minutes < 2 ? 'minute' : 'minutes'}'` : ''}`
             )
         )
     } else if (timeDiff.minutes > 0) {
         return (
             timeDiff.toFormat(
-                `m '${timeDiff.minutes === 1 ? 'minute' : 'minutes'}'`
+                `m '${timeDiff.minutes < 2 ? 'minute' : 'minutes'}'`
             )
         )
     } else if (timeDiff.seconds >= 0) {
         return (
            timeDiff.toFormat(
-            `s ${timeDiff.seconds < 2 ? "'s'econ'd'" : "'s'econ'ds'"}`
+            `s ${timeDiff.seconds < 2 ? 'second' : 'seconds'}`
            )
         )
     } else if (timeDiff.seconds < 0 && timeDiff.seconds > -3600 ) {
